8ede78602d52b04498c86d0898c83b6080c18b9a
mashok2 <mashok2@caltech.edu>
2024-08-13 16:53:34 -0700

    Some characteristic parameters that predict ability to reorganise added in example (see chapter 4.1 in Mengsha's thesis). Parameters set to order of magnitudes accordingly. Need to debug to remove instability.

--- example_usage.m
+++ example_usage.m
@@ -7,15 +7,19 @@
 
 %% Declare Parameters
 contraction_rate = 20;  % Contraction rate of jellyfish (contrations per minute). Parameter measured from experiments, see figure S6 in Supplementary Material.
-muscle_strain = 0.2;    % Muscle strain of jellyfish (dimensionless). Parameter measurement from experiments, see figure S6 in Supplementary Materials.      
+muscle_strain = 0.21;    % Muscle strain of jellyfish (dimensionless). Parameter measurement from experiments, see figure S6 in Supplementary Materials.      
 offset = 2;            % Offset of jellyfish grafts
 
-bulk_modulus = 0.1;
+bulk_modulus = 2e6;      
 area = 1.001;           % Initial area of jellyfish.
-elast0 = 0.05;          % Elasticity of spring. 
-elast1 = 0.05;          % Elasticity of spring in Maxwell model. 
-vis = 1000;              % Dashpot in Maxwell model. 
+elast0 = 5;            % (Pa) Elasticity of spring. 
+elast1 = 15;            % (Pa) Elasticity of spring in Maxwell model. 
+vis = 1e5;             % (Pa.s) Dashpot in Maxwell model. 
 
+%% Parameters to predict whether shape will reorganise, see Chapter 4.1 in M. Gong's thesis (2022). 
+characteristic_ratio = muscle_strain*(elast0 + elast1)/vis;     % Should be betweeen 0.4e-6 and 0.3e-5 (1/s) for full reorganisation. 
+relaxation_time = (vis/elast1)/3600;                            % Relaxation time greater than 11.6 hours did not fully reorganise.
+elasticity_ratio = elast0/elast1;                               % 1:4 ratio resulted in full reorganisation, whereas 1:1 ratio did not.
 %% Simulation 
 
 % Specify datapath of directory to save data in
