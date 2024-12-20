3a3243f989f1fb319cc74dba4804e87c29b4d545
mashok2 <mashok2@caltech.edu>
2024-12-20 11:00:36 -0800

    Slurm test script updated.

--- slurm_test_script.m
+++ slurm_test_script.m
@@ -1,3 +1,9 @@
 % To test slurm job set-up
 
 print("Successful Run.")
+
+%% Writing images
+path0 = pwd();
+folder_save = 'test_slurm_script'; 
+Dr = dir([path0 '/' folder_save]);
+S = mkdir(path0,folder_save);  
