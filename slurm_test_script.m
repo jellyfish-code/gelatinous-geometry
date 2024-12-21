92e7120cca22c273dda3c1c1478a77a916eead1b
Minakshi Ashok <mashok2@login3.cm.cluster>
2024-12-20 17:16:17 -0800

    slurm files updated.

--- slurm_test_script.m
+++ slurm_test_script.m
@@ -1,9 +1,11 @@
 % To test slurm job set-up
 
-print("Successful Run.")
+disp("Successful Run.")
 
 %% Writing images
 path0 = pwd();
 folder_save = 'test_slurm_script'; 
 Dr = dir([path0 '/' folder_save]);
+disp("directory"); 
+disp(Dr);
 S = mkdir(path0,folder_save);  
