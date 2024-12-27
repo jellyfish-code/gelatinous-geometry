b303c9ee3ce7708bdc0289816272ac9527423cb9
mashok2 <mashok2@caltech.edu>
2024-12-26 16:52:02 -0800

    Changes to example usage.

--- example_usage.m
+++ example_usage.m
@@ -12,8 +12,8 @@ offset = 4;                 % Offset of jellyfish grafts
 
 bulk_modulus = 0.05*1e3;    % Bulk modulus of jellyfish in Pascals. 
 area0 = 1.001;              % Relaxed area as a percentage of jellyfish area. Greater than 1.
-elast0 = 0.05*0.2*1e3;      % Elasticity of spring in Pascals.
-elast1 = 0.05*0.8*1e3;      % Elasticity of spring in Pascals.
+elast0 = 0.025*0.2*1e3;      % Elasticity of spring in Pascals.
+elast1 = 0.025*0.8*1e3;      % Elasticity of spring in Pascals.
 vis = 500*1e3;              % Viscosity of dashpot in Maxwell model. Units in Pascal*seconds.
 damping_coefficient = 500;  % Damping coefficient of jellyfish. Units in Newton*seconds/meter.
 max_dR = 1.55;              % Maximum change in radius of jellyfish during contraction.
@@ -25,25 +25,26 @@ tau = min(tau_SLM, tau_maxwell);
 timestep_fraction_of_tau = tau/(30*60); % current timestep is 30 mins
 
 %% Uncomment to simulate an offset graft
-% % Specify datapath of directory to save data in
-% datapath = pwd; % Set current directory as datapath
-% graft_type = '_offset_graft'; 
-% % Specify subfolder to save data in. Data is saved in datapath (here, the current directory) inside folder Data. Creates folder example_usage_data if not already created.
-% date = sprintf('%s', datetime("today"));  
-% folder_save = ['example_usage_data/', date, graft_type, '_elast0_', num2str(elast0), '_elast1_', num2str(elast1), '_viscosity_', num2str(vis), '_bulk_modulus_', num2str(bulk_modulus), '_offset_', num2str(offset), '_contraction_rate_', num2str(contraction_rate)];
-% 
-% visco_offset_SLM_newmus(elast0, elast1, vis, damping_coefficient, bulk_modulus, area0, muscle_strain, contraction_rate, max_dR, dR_rate, offset, folder_save, datapath)
-
-%% Uncomment to simulate a butterfly graft
 % Specify datapath of directory to save data in
 datapath = pwd; % Set current directory as datapath
-graft_type = '_butterfly_graft'; 
-% Specify subfolder to save data in. Data is saved in datapath (here, the current directory) inside folder Data. 
-% Creates folder Data if not already created.
+graft_type = '_offset_graft'; 
+% Specify subfolder to save data in. Data is saved in datapath (here, the current directory) inside folder Data. Creates folder example_usage_data if not already created.
 date = sprintf('%s', datetime("today"));  
 folder_save = ['example_usage_data/', date, graft_type, '_elast0_', num2str(elast0), '_elast1_', num2str(elast1), '_viscosity_', num2str(vis), '_bulk_modulus_', num2str(bulk_modulus), '_offset_', num2str(offset), '_contraction_rate_', num2str(contraction_rate)];
 
-visco_butterfly_SLM_newmus(elast0, elast1, vis, damping_coefficient, bulk_modulus, area0, muscle_strain, contraction_rate, max_dR, dR_rate, folder_save, datapath)
+visco_offset_SLM_newmus(elast0, elast1, vis, damping_coefficient, bulk_modulus, area0, muscle_strain, contraction_rate, max_dR, dR_rate, offset, folder_save, datapath)
+
+%% Uncomment to simulate
+% a butterfly graft
+% % Specify datapath of directory to save data in
+% datapath = pwd; % Set current directory as datapath
+% graft_type = '_butterfly_graft'; 
+% % Specify subfolder to save data in. Data is saved in datapath (here, the current directory) inside folder Data. 
+% % Creates folder Data if not already created.
+% date = sprintf('%s', datetime("today"));  
+% folder_save = ['example_usage_data/', date, graft_type, '_elast0_', num2str(elast0), '_elast1_', num2str(elast1), '_viscosity_', num2str(vis), '_bulk_modulus_', num2str(bulk_modulus), '_offset_', num2str(offset), '_contraction_rate_', num2str(contraction_rate)];
+% 
+% visco_butterfly_SLM_newmus(elast0, elast1, vis, damping_coefficient, bulk_modulus, area0, muscle_strain, contraction_rate, max_dR, dR_rate, folder_save, datapath)
 
 %% (Optional) Stitch images into a .avi video
 dataDir = fullfile([pwd, '/' ,folder_save]); % Specify location of images
